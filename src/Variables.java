a5d49a2301b04269c832bb021dafb34021c0caa6
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-12 16:37:57 -0600

    variable lesson

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,12 @@
 public class Variables {
-}
+    public static void main(String[] args) {
+        //VARIABLES:
+        int age = 30, temperature = 20;
+        System.out.println(age);
+        System.out.println(temperature);
+
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+
+}}
