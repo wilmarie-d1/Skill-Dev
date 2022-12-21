625779e8aa3cb2c828fd8387df5e27db92a9abe8
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-21 12:44:17 -0600

    functions exercise

---
+++ Udemy-Functions.js
@@ -0,0 +1,11 @@
+// Heart Function Exercise
+// It's time to write our first function!
+//
+// Define a function called printHeart that prints out the string "<3"
+//
+// Execute your function once
+
+function printHeart() {
+    console.log("<3")
+}
+printHeart();
