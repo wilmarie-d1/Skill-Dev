1855fcfb255024c2006649e8ade4a449850abd01
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2022-12-20 23:23:42 -0600

    java video exercise

--- src/Variables.java
+++ src/Variables.java
@@ -1,12 +1,33 @@
+import java.awt.*;
+import java.util.Date;
+
 public class Variables {
     public static void main(String[] args) {
         //VARIABLES:
-        int age = 30, temperature = 20;
-        System.out.println(age);
-        System.out.println(temperature);
+//        int age = 30, temperature = 20;
+//        System.out.println(age);
+//        System.out.println(temperature);
 
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+//        int myAge = 30;
+//        int herAge = myAge;
+//        System.out.println(herAge);
 
-}}
+        //PRIMITIVE TYPES
+        byte age = 30;
+        long viewsCount = 123456789L;
+        double price1 = 10.99;
+        float price = 10.99f;
+        char letter = 'A';
+        boolean isEligible = true;
+
+        //REFERENCE TYPES
+         byte Age = 30;
+        Date now = new Date();
+        now.getTime(); //returns the time component of this object
+        System.out.println(now); //prints out the value
+
+     //POINTS
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point1.x = 2;
+    }}
