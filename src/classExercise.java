2cb3acf88e48f4be0d13d15723d8d8eeff190cf2
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-04 15:20:56 -0600

    class exercise setup

---
+++ src/classExercise.java
@@ -0,0 +1,17 @@
+public class classExercise {
+    public static void main(String[] args) {
+        groceryStore houstonStore = new groceryStore();
+        groceryStore seattleStore = new groceryStore();
+        groceryStore orlandoStore = new groceryStore();
+    }
+
+}
+
+class groceryStore {
+    int applesSoldYearly;
+    double appleRetailPrice;
+     int orangesSoldYearly;
+    double orangeRetailPrice;
+
+
+}
