c08519e2e548e3ba8f0f0fbd144047a69a762782
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-09 10:50:26 -0600

    constructor exercise

--- src/classExercise.java
+++ src/classExercise.java
@@ -1,32 +1,22 @@
 public class classExercise {
     public static void main(String[] args) {
-        groceryStore houstonStore = new groceryStore();
-        groceryStore seattleStore = new groceryStore();
-        groceryStore orlandoStore = new groceryStore();
+        groceryStore houstonStore = new groceryStore(534, 0.99, 429, 0.87);
+        groceryStore seattleStore = new groceryStore(765, 0.86, 842, 0.91);
+        groceryStore orlandoStore = new groceryStore(402, 0.77, 398, 0.79);
 
-        houstonStore.applesSoldYearly = 534;
-        houstonStore.appleRetailPrice =0.99;
-        houstonStore.orangesSoldYearly = 429;
-        houstonStore.orangeRetailPrice = 0.87;
+
         System.out.println("Houston Store");
         System.out.println("\nThe amount of apples sold in Houston yearly is: " + houstonStore.applesSoldYearly);
         System.out.println("The retail price of apples sold in Houston is: " + houstonStore.appleRetailPrice);
         System.out.println("\n The amount of oranges sold in Houston yearly is: " + houstonStore.orangesSoldYearly);
         System.out.println("The retail price of oranges sold in Houston is: " + houstonStore.orangeRetailPrice);
-        seattleStore.applesSoldYearly = 765;
-        seattleStore.appleRetailPrice = 0.86;
-        seattleStore.orangesSoldYearly = 842;
-        seattleStore.orangeRetailPrice = 0.91;
+
         System.out.println("\n Seattle Store");
         System.out.println("\n The amount of apples sold in Seattle yearly is: " + seattleStore.applesSoldYearly);
         System.out.println("The retail price of oranges sold in Seattle is: " + seattleStore.appleRetailPrice);
         System.out.println("\n The amount of oranges sold in Seattle yearly is: " + seattleStore.orangesSoldYearly);
         System.out.println("The retail price of oranges sold in Seattle is: " + seattleStore.orangeRetailPrice);
 
-        orlandoStore.applesSoldYearly = 402;
-        orlandoStore.appleRetailPrice = 0.77;
-        orlandoStore.orangesSoldYearly = 398;
-        orlandoStore.orangeRetailPrice = 0.79;
         System.out.println("\n Seattle Store");
         System.out.println(" \n The amount of apples sold in Orlando yearly is: " + orlandoStore.applesSoldYearly);
         System.out.println("The retail price of apples sold in Orlando is: " + orlandoStore.appleRetailPrice);
@@ -43,18 +33,24 @@ public class classExercise {
 
 
         System.out.println("Orlando Store");
-        System.out.println(orlandoStore.grossRevenue());
+        System.out.println("To hit the target of $1000, the store needs to sell " + orlandoStore.appleRevenueTarget(1000) + "apples");
+        System.out.println("To hit the target of $800, the store needs to sell " + orlandoStore.orangeRevenueTarget(800) + "oranges");
+        System.out.println("The gross revenue for this store is " + orlandoStore.grossRevenue());
         System.out.println("Houston Store");
-        System.out.println(houstonStore.grossRevenue());
+        System.out.println("To hit the target of $1000, the store needs to sell " + houstonStore.appleRevenueTarget(1000) + "apples");
+        System.out.println("To hit the target of $800, the store needs to sell " + houstonStore.orangeRevenueTarget(800) + "apples");
+        System.out.println("The gross revenue for this store is " + houstonStore.grossRevenue());
         System.out.println("Seattle Store");
-        System.out.println(seattleStore.grossRevenue());
-
-        System.out.println("Houston target is $1050 of apple sales..");
-        System.out.println("It needs to sell " + houstonStore.appleRevenueTarget(1050) + " apples");
-        System.out.println("\nSeattle target is 620 of apple sales..");
-        System.out.println("It needs to sell " + seattleStore.orangeRevenueTarget(620) + " oranges");
-        System.out.println("\nOrlando target is $744 of apple sales..");
-        System.out.println("It needs to sell " + orlandoStore.appleRevenueTarget(744) + " apples");
+        System.out.println("To hit the target of $1000, the store needs to sell " + seattleStore.appleRevenueTarget(1000) + "apples");
+        System.out.println("To hit the target of $800, the store needs to sell " + seattleStore.orangeRevenueTarget(800) + "oranges");
+        System.out.println("The gross revenue for this store is " + seattleStore.grossRevenue());
+//
+//        System.out.println("Houston target is $1050 of apple sales..");
+//        System.out.println("It needs to sell " + houstonStore.appleRevenueTarget(1050) + " apples");
+//        System.out.println("\nSeattle target is 620 of apple sales..");
+//        System.out.println("It needs to sell " + seattleStore.orangeRevenueTarget(620) + " oranges");
+//        System.out.println("\nOrlando target is $744 of apple sales..");
+//        System.out.println("It needs to sell " + orlandoStore.appleRevenueTarget(744) + " apples");
 
     }
 
@@ -66,6 +62,12 @@ class groceryStore {
     int orangesSoldYearly;
     double orangeRetailPrice;
 
+    groceryStore(int asy, double arp, int osy, double orp) {
+        applesSoldYearly = asy;
+        appleRetailPrice = arp;
+        orangesSoldYearly = osy;
+        orangeRetailPrice = orp;
+    }
     double grossRevenue () {
         double revenue;
         revenue = (applesSoldYearly * appleRetailPrice) + (orangesSoldYearly * orangeRetailPrice);
