84851d94c505b19165eedd8ede5fc60d59a63a40
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-09 10:21:47 -0600

    constructor exercise

--- src/Lecture.java
+++ src/Lecture.java
@@ -1,19 +1,8 @@
 public class Lecture {
 
     public static void main(String[] args) {
-        aircraft cessna172 = new aircraft();
-        aircraft piperSaratoga = new aircraft();
-
-        cessna172.passengers = 4;
-        cessna172.cruiseSpeed = 140;
-        cessna172.fuelCapacity = 56.5;
-        cessna172.fuelBurnRate = 9.5;
-
-        piperSaratoga.passengers = 6;
-        piperSaratoga.cruiseSpeed =201;
-        piperSaratoga.fuelCapacity = 102.5;
-        piperSaratoga.fuelBurnRate = 20.5;
-
+        aircraft cessna172 = new aircraft(94, 140, 56.6, 9.5);
+        aircraft piperSaratoga = new aircraft(6, 201, 102.5, 20.5);
 
         System.out.println("For Cessna172 to fly up to 4.2 hours, it takes " + cessna172.fuelNeeded(4.2) + "gallons");
         System.out.println("For Saratoga to fly up to 7 hours, it takes " + piperSaratoga.fuelNeeded(4.2) + "gallons");
@@ -25,6 +14,13 @@ public class Lecture {
     int cruiseSpeed;
     double fuelCapacity;
     double fuelBurnRate;
+
+    aircraft (int p, int c, double fc, double fbr) {
+        passengers = p;
+        cruiseSpeed = c;
+        fuelCapacity = fc;
+        fuelBurnRate = fbr;
+    }
 
 
     double calculateEndurance () {
