fec8d5dbac2e745a374541000a5e79d35981dc83
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-01-25 10:21:48 -0600

    java exercises

---
+++ src/Basic_Exercise.java
@@ -0,0 +1,20 @@
+public class Basic_Exercise {
+
+    public static void main(String[] args) {
+        System.out.println("Hello\nWilmarie De La Cruz!");
+        System.out.println(sum(19, 20));
+        System.out.println(div(60, 12));
+        System.out.println(product(15, 34));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int div(int num1, int num2) {
+        return num1 / num2;
+    }
+    public static int product(int num1, int num2){
+        return  num1*num2;
+    }
+}
