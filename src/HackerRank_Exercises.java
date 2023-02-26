eee2614b9baad3b9ce88ed2384ea280287ab17ea
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-02-25 21:30:33 -0600

    first exercise

---
+++ src/HackerRank_Exercises.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HackerRank_Exercises {
+//    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+    // Complete this line
+    // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    // Complete this line
+    // Complete this line
+}
+
+}
